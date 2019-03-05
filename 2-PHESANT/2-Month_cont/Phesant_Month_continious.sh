6b7954cad7cd7feb6465c1381cbb3b11cbf0a297
Beate Leppert <bl17303@newblue1.cm.cluster>
2019-03-05 10:09:20 +0000

    Add job file for phesant with month as continious variable, starting with September (0)

---
+++ 2-PHESANT/2-Month_cont/Phesant_Month_continious.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#PBS -l walltime=360:00:00,nodes=1:ppn=1
+#PBS -S /bin/bash
+#PBS -e errors
+#PBS -t 1-100
+#------------------------------------------------------------------------------------
+
+module add languages/R-3.3.1-ATLAS
+
+pIdx=${PBS_ARRAYID}
+np=100
+
+homedir="${HOME}/Phesant_ageatschool/"
+
+date
+
+cd ../../../4-phesant/WAS/
+Rscript phenomeScan.r \
+--phenofile="${homedir}data/data_21753-phesant_header.csv" \
+--traitofinterestfile="${homedir}data/exposure/exposure_1.csv" \
+--confounderfile="${homedir}data/confounder/confounders-sex_age_asscen.csv" \
+--variablelistfile="${homedir}4-phesant/variable-info/outcome-info.tsv" \
+--datacodingfile="${homedir}4-phesant/variable-info/data-coding-ordinal-info.txt" \
+--resDir="${homedir}4-phesant/2-Month_cont/" \
+--userId="eid" \
+--traitofinterest="recode_month" \
+--genetic=FALSE \
+--partIdx=$pIdx \
+--numParts=$np
+
+
+
+date

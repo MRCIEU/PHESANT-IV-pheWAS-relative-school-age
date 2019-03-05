50e831676bba5393782fb061375d78b511210171
Beate Leppert <bl17303@newblue1.cm.cluster>
2019-03-05 10:05:05 +0000

    Add job file for phesant comparing August vs September births

---
+++ 2-PHESANT/1-AUG_SEP/Phesant_Aug_Sep.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#PBS -l walltime=360:00:00,nodes=1:ppn=1
+#PBS -S /bin/bash
+#PBS -e errors
+#PBS -t 1-100
+#------------------------------------------------------------------------------------
+
+module add languages/R-3.3.1-ATLAS
+
+pIdx=${PBS_ARRAYID}
+np=100
+
+homedir="${HOME}/Phesant_ageatschool/"
+
+date
+
+cd ../../../4-phesant/WAS/
+Rscript phenomeScan.r \
+--phenofile="${homedir}data/data_21753-phesant_header.csv" \
+--traitofinterestfile="${homedir}data/exposure/exposure.csv" \
+--confounderfile="${homedir}data/confounder/confounders-sex_age_asscen.csv" \
+--variablelistfile="${homedir}4-phesant/variable-info/outcome-info.tsv" \
+--datacodingfile="${homedir}4-phesant/variable-info/data-coding-ordinal-info.txt" \
+--resDir="${homedir}4-phesant/1-Aug_Sep/" \
+--userId="eid" \
+--traitofinterest="Aug_Sep" \
+--genetic=FALSE \
+--partIdx=$pIdx \
+--numParts=$np
+
+
+
+date

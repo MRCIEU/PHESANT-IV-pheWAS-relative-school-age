4631dfea4047640b3df8a91f59611d61d06bc49d
Beate Leppert <bl17303@newblue1.cm.cluster>
2019-03-05 09:56:16 +0000

    Add job files for phesant with month indicators

---
+++ PHESANT-with-month-indicators/jobs/job166.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#PBS -l walltime=360:00:00,nodes=1:ppn=4
+#PBS -o output-all
+#PBS -e errors-all
+#PBS -t 166
+#---------------------------------------------
+
+cd $PBS_O_WORKDIR
+
+module add languages/R-3.3.1-ATLAS
+
+date
+
+
+dataDir="${HOME}/Phesant_ageatschool/data/"
+resultsDir="${HOME}/Phesant_ageatschool/4-phesant/3-Month_cat/"
+
+expFile="${dataDir}exposure/exposure_2.csv"
+confFile="${dataDir}confounder/confounders-sex_age_asscen.csv"
+
+phenodir="${dataDir}derived_phenotypes/"
+
+part=${PBS_ARRAYID}
+np=200
+
+cd ..
+Rscript testFromSaved.r --userId="eid" --resDir=${resultsDir} --partIdx=${part} --numParts=${np} --confounderfile=${confFile} --phenoDir=${phenodir} --traitofinterestfile=${expFile}
+
+date 

481d86d970d34302af875e756dc9632204b6ee61
Beate Leppert <bl17303@newblue4.cm.cluster>
2019-03-07 16:57:06 +0000

    Correct file paths

--- 1-data_preparation/make_confounder.r
+++ 1-data_preparation/make_confounder.r
@@ -1,12 +1,12 @@
 homedir = Sys.getenv("HOME")
 
-datadir=(paste0(homedir,"/Phesant_ageatschool/data/confounder")
+datadir=(paste0(homedir,"/Phesant_ageatschool/data/confounder"))
 
 ### Generate confounder file
 ### Adjusting for age, sex and assessment centre
 
 # age, sex, assessment centre
-confs = read.table(paste(phenodatadir,'/variables_for_confounder.csv', sep=""), sep=',', header=1)
+confs = read.table(paste(datadir,'/variables_for_confounder.csv', sep=""), sep=',', header=1)
 colnames(confs) = c("eid","x31_0_0","x52_0_0","x54_0_0","x54_1_0","x54_2_0","x1647_1_0","x1647_2_0","x1647_3_0","x21022_0_0")
 print(dim(confs))
 
@@ -51,5 +51,5 @@ for (i in 11001:11023) {
 myconf = confs[,c("eid","x31_0_0","x21022_0_0","assCentre11001","assCentre11002","assCentre11003","assCentre11004","assCentre11005","assCentre11006","assCentre11007","assCentre11008","assCentre11009","assCentre11010","assCentre11011","assCentre11012","assCentre11013","assCentre11014","assCentre11016","assCentre11017","assCentre11018","assCentre11020","assCentre11021","assCentre11022","assCentre11023")]
 
 
-write.table(myconf, paste(phenodatadir,'/confounders-sex_age_asscen.csv',sep=""), sep=',', row.names=FALSE, quote = FALSE)
+write.table(myconf, paste(datadir,'/confounders-sex_age_asscen.csv',sep=""), sep=',', row.names=FALSE, quote = FALSE)
 

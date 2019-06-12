297733a10194d934f73c04f66b233a85b64fcb77
Beate Leppert <bl17303@newblue1.cm.cluster>
2019-06-12 13:23:58 +0100

    Add week of birth (wob) to exposures

--- 1-data_preparation/make_exposure.r
+++ 1-data_preparation/make_exposure.r
@@ -1,4 +1,6 @@
 homedir = Sys.getenv('HOME')
+wobdir = Sys.getenv('WOBDIR')
+
 
 library("data.table")
 
@@ -114,5 +116,35 @@ exp_fin_2 = exp_sub_2[,c("eid","Oct","Nov","Dec","Jan","Feb","Mar","May","Jun","
 head(exp_fin_2)
 write.csv(exp_fin_2, paste0(homedir,"/Phesant_ageatschool/data/exposure/exposure_2.csv"), row.names=F,quote=F)
 
+#######################################################
+## Week of Birth #####################################
 
+# For August - Spetember births 
 
+wob = read.csv(paste0(wobdir,"/app_16729_wob.csv"))
+head(wob)
+
+wob_exp = merge(exp_fin, wob, keep.all=TRUE)
+wob_exp = wob_exp[order(wob_exp$week),]
+unique(wob_exp$week)
+
+#31 32 33 34 35 36 37 38 39
+
+wob_fin = wob_exp[,c(1,3)]
+write.csv(wob_fin, paste0(homedir,"/Phesant_ageatschool/data/exposure/exposure_wob_1.csv"), row.names=F,quote=F)
+
+# Continious for all
+
+wob_cont = merge(exp_fin_1, wob, keep.all=TRUE)
+wob_cont = wob_cont[order(wob_cont$week),]
+wob_cont = wob_cont[order(wob_cont$recode_month),]
+
+wob_cont["week_new"] = wob_cont$week
+
+wob_cont$week_new[wob_cont$week == 35 & wob_cont$recode_month == 0] = 0
+wob_cont$week_new[wob_cont$week == 35 & wob_cont$recode_month == 11] = 52
+
+wob_cont$week_new =recode(wob_cont$week_new,"36=1;37=2;38=3;39=4;40=5;41=6;42=7;43=8;44=9;45=10;46=11;47=12;48=13;49=14;50=15;51=16;52=17;1=18;2=19;3=20;4=21;5=22;6=23;7=24;8=25;9=26;10=27;11=28;12=29;13=30;14=31;15=32;16=33;17=34;18=35;19=36;20=37;21=38;22=39;23=40;24=41;25=42;26=43;27=44;28=45;29=46;30=47;31=48;32=49;33=50;34=51")
+
+wob_cont_fin = wob_cont[,c(1,4)]
+write.csv(wob_cont_fin, paste0(homedir,"/Phesant_ageatschool/data/exposure/exposure_wob_2.csv"), row.names=F,quote=F)

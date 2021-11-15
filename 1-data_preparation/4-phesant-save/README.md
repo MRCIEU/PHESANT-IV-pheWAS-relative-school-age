1f5a9ad712a5661b7fc699f8b9c3d71d8f5613d3
Louise Millard <louise.millard@bristol.ac.uk>
2021-11-15 14:47:40 +0000

    add phesant job scripts to derive phenotype data

---
+++ 1-data_preparation/4-phesant-save/README.md
@@ -0,0 +1,9 @@
+
+# PHESANT-derived data
+
+Derive cleaned phenotypes using PHESANT, on Blue Crystal phase 3.
+
+```bash
+qsub j1.sh
+qsub j2.sh
+```
---
+++ 1-data_preparation/4-phesant-save/j1.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#PBS -l walltime=360:00:00,nodes=1:ppn=1
+#PBS -o output
+#PBS -e errors
+#PBS -t 1-100
+#---------------------------------------------
+
+
+
+module add languages/R-4.0.3-gcc9.1.0 
+
+date
+
+dataDir="${HOME}/2019-school-age-for-kate/data/"
+codeDir="${HOME}/2019-school-age-for-kate/code/PHESANT/WAS/"
+varListDir="${HOME}/2019-school-age-for-kate/code/PHESANT/variable-info/"
+
+
+outcomeFile="${dataDir}/data.48196.phesant.csv"
+varListFile="${varListDir}outcome-info.tsv"
+dcFile="${varListDir}data-coding-ordinal-info.txt"
+resDir="${HOME}/2019-school-age-for-kate/data/PHESANT-saved/"
+
+# start and end index of phenotypes
+pIdx=${PBS_ARRAYID}
+np=200
+
+cd $codeDir
+Rscript ${codeDir}phenomeScan.r --partIdx=$pIdx --numParts=$np --phenofile=${outcomeFile} --variablelistfile=${varListFile} --datacodingfile=${dcFile} --resDir=${resDir} --userId="eid" --save
+
+
+
+date
+
+
+

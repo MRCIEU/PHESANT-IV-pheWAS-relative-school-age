40fa5fa66d486533614836e373e44284b75c80ff
Mel de Lange <bo18534@bc4login3.bc4.acrc.priv>
2022-03-08 14:44:02 +0000

    Updated phenotype extracting to most recent data

--- 1-data_preparation/1-extract_Exp_Conf.sh
+++ 1-data_preparation/1-extract_Exp_Conf.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
-#PBS -l walltime=24:00:00,nodes=1:ppn=4
-#PBS -S /bin/bash
+
+#SBATCH --job-name=Extract_Phenotypes
+#SBATCH --partition=veryshort
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=1:0:00
+#SBATCH --mem=100G
+
+echo 'Extract_Phenotypes'
+srun hostname
+
 
 #------------------------------------------------------------------------------------
 
-
-head -n 1 ${PROJECTDATADIR}/data.21753.csv | sed 's/,/\n/g' | cat -n | grep '31-'
-head -n 1 ${PROJECTDATADIR}/data.21753.csv | sed 's/,/\n/g' | cat -n | grep '52-'
-head -n 1 ${PROJECTDATADIR}/data.21753.csv | sed 's/,/\n/g' | cat -n | grep '54-'
-head -n 1 ${PROJECTDATADIR}/data.21753.csv | sed 's/,/\n/g' | cat -n | grep '21022-'
-head -n 1 ${PROJECTDATADIR}/data.21753.csv | sed 's/,/\n/g' | cat -n | grep '1647-'
+export PROJECT_DATA="/mnt/storage/scratch/bo18534/SchoolAgeMiniProject/"
 
 
-cut -d ',' -f 1,9,32,36,37,38,751,752,753,7075 ${PROJECTDATADIR}/data.21753.csv > ${HOME}/Phesant_ageatschool/data/confounder/variables_for_confounder.csv
+#head -n 1 ${PROJECT_DATA}/Original/data.48196.phesant.csv | sed 's/,/\n/g' | cat -n | grep 'x31_'
+#head -n 1 ${PROJECT_DATA}/Original/data.48196.phesant.csv | sed 's/,/\n/g' | cat -n | grep 'x52_'
+#head -n 1 ${PROJECT_DATA}/Original/data.48196.phesant.csv | sed 's/,/\n/g' | cat -n | grep 'x54_'
+#head -n 1 ${PROJECT_DATA}/Original/data.48196.phesant.csv | sed 's/,/\n/g' | cat -n | grep 'x21022_'
+#head -n 1 ${PROJECT_DATA}/Original/data.48196.phesant.csv | sed 's/,/\n/g' | cat -n | grep 'x1647_'
 
-
+cut -d ',' -f 11,41,46,47,48,49,9093,1009,1010,1011 ${PROJECT_DATA}/Original/data.48196.phesant.csv > ${PROJECT_DATA}/Derived/exposure_confounder_variables.csv
 

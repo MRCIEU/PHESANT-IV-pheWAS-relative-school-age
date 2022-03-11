f8c0f61b11d6321766c1ce5630de86b529977e9a
Mel de Lange <bo18534@bc4login3.bc4.acrc.priv>
2022-03-11 13:43:03 +0000

    Updated phenotype extracting to include eid field

--- 1-data_preparation/1-extract_Exp_Conf.sh
+++ 1-data_preparation/1-extract_Exp_Conf.sh
@@ -22,6 +22,7 @@ export PROJECT_DATA="/mnt/storage/scratch/bo18534/SchoolAgeMiniProject/"
 #head -n 1 ${PROJECT_DATA}/Original/data.48196.phesant.csv | sed 's/,/\n/g' | cat -n | grep 'x54_'
 #head -n 1 ${PROJECT_DATA}/Original/data.48196.phesant.csv | sed 's/,/\n/g' | cat -n | grep 'x21022_'
 #head -n 1 ${PROJECT_DATA}/Original/data.48196.phesant.csv | sed 's/,/\n/g' | cat -n | grep 'x1647_'
+#head -n 1 ${PROJECT_DATA}/Original/data.48196.phesant.csv | sed 's/,/\n/g' | cat -n | grep 'eid'
 
-cut -d ',' -f 11,41,46,47,48,49,9093,1009,1010,1011 ${PROJECT_DATA}/Original/data.48196.phesant.csv > ${PROJECT_DATA}/Derived/exposure_confounder_variables.csv
+cut -d ',' -f 1,11,41,46,47,48,49,9093,1009,1010,1011 ${PROJECT_DATA}/Original/data.48196.phesant.csv > ${PROJECT_DATA}/Derived/exposure_confounder_variables.csv
 

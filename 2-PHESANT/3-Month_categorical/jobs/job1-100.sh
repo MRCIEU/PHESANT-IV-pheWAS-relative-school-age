e69b8f2d6ca14c57e6e2d69314dcb3962ca532ff
MeldeLange <bo18534@bristol.ac.uk>
2022-03-21 13:05:38 +0000

    Updated directories

--- 2-PHESANT/3-Month_categorical/jobs/job1-100.sh
+++ 2-PHESANT/3-Month_categorical/jobs/job1-100.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=5-00:0:00
+#SBATCH --time=01:00:00
 #SBATCH --mem=100G
 #SBATCH --array=1-100
 echo 'phesant_exposure2'
@@ -15,25 +15,27 @@ srun hostname
 #------------------------------------------------------------------------------------
 
 
-cd $PBS_O_WORKDIR
+cd $SLURM_SUBMIT_DIR
 
-module add languages/R-3.3.1-ATLAS
+module load languages/r/4.1.0
 
 date
 
-dataDir = Sys.getenv("PROJECT_DATA")
-resDir = Sys.getenv("RES_DIR")
+export RES_DIR="${HOME}/2022SchoolAge/results/phesant/exposure2/"
+export PROJECT_DATA="/mnt/storage/scratch/bo18534/SchoolAgeMiniProject/"
 
 
-expFile="${dataDir}Derived/exposure2_month_indicator_variables.csv"
-confFile="${dataDir}Derived/confounders-sex_age.csv"
 
-phenodir="${dataDir}derived_phenotypes/"
+expFile="${PROJECT_DATA}Derived/exposure2_month_indicator_variables.csv"
+confFile="${PROJECT_DATA}Derived/confounders-sex_age.csv"
+
+phenodir="${PROJECT_DATA}/Original/phesant_saved-20211117/"
 
 part=${SLURM_ARRAY_TASK_ID}
 np=200
 
-cd ..
-Rscript ../PHESANT_from_saved/testFromSaved.r --userId="eid" --resDir=${resultsDir} --partIdx=${part} --numParts=${np} --confounderfile=${confFile} --phenoDir=${phenodir} --traitofinterestfile=${expFile}
+cd $HOME/2022SchoolAge/code/PHESANT-IV-pheWAS-relative-school-age/2-PHESANT/PHESANT_from_saved/
+
+Rscript testFromSaved.r --userId="eid" --resDir=${RES_DIR} --partIdx=${part} --numParts=${np} --confounderfile=${confFile} --phenoDir=${phenodir} --traitofinterestfile=${expFile}
 
 date 

e8e1f576f587451a2ae03d357c8f88f50158a9de
MeldeLange <bo18534@bristol.ac.uk>
2022-03-21 15:01:10 +0000

    Updated to BC4 & new directories

--- 2-PHESANT/3-Month_categorical/jobs/job101-200.sh
+++ 2-PHESANT/3-Month_categorical/jobs/job101-200.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
-#PBS -l walltime=360:00:00,nodes=1:ppn=1
-#PBS -o output-all
-#PBS -e errors-all
-#PBS -t 101-158,160-165,167-200
-#---------------------------------------------
 
-cd $PBS_O_WORKDIR
+#SBATCH --job-name=phesant_exposure2
+#SBATCH --partition=cpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=02:00:00
+#SBATCH --mem=100G
+#SBATCH --array=101-200
+echo 'phesant_exposure2'
+srun hostname
 
-module add languages/R-3.3.1-ATLAS
+
+#------------------------------------------------------------------------------------
+
+
+cd $SLURM_SUBMIT_DIR
+
+module load languages/r/4.1.0
+
+date
+
+export RES_DIR="${HOME}/2022SchoolAge/results/phesant/exposure2/"
+export PROJECT_DATA="/mnt/storage/scratch/bo18534/SchoolAgeMiniProject/"
+
+
+expFile="${PROJECT_DATA}Derived/exposure2_month_indicator_variables.csv"
+confFile="${PROJECT_DATA}Derived/confounders-sex_age.csv"
+
+phenodir="${PROJECT_DATA}/Original/phesant_saved-20211117/"
+
+part=${SLURM_ARRAY_TASK_ID}
+np=200
+
+cd $HOME/2022SchoolAge/code/PHESANT-IV-pheWAS-relative-school-age/2-PHESANT/PHESANT_from_saved/
+
+Rscript testFromSaved.r --userId="eid" --resDir=${RES_DIR} --partIdx=${part} --numParts=${np} --confounderfile=${confFile} --phenoDir=${phenodir} --traitofinterestfile=${expFile}
 
 date
 
 
-dataDir="${HOME}/Phesant_ageatschool/data/"
-resultsDir="${HOME}/Phesant_ageatschool/4-phesant/3-Month_cat/"
 
-expFile="${dataDir}exposure/exposure_2.csv"
-confFile="${dataDir}confounder/confounders-sex_age_asscen.csv"
-
-phenodir="${dataDir}derived_phenotypes/"
-
-part=${PBS_ARRAYID}
-np=200
-
-cd ..
-Rscript testFromSaved.r --userId="eid" --resDir=${resultsDir} --partIdx=${part} --numParts=${np} --confounderfile=${confFile} --phenoDir=${phenodir} --traitofinterestfile=${expFile}
-
-date 

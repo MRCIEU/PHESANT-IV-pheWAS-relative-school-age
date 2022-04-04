458431f2d1423036fb571a16a1d5d6815b7b0efe
MeldeLange <bo18534@bristol.ac.uk>
2022-04-04 15:55:55 +0100

    Updated sample sizes once withdrawn cases removed

--- 1-data_preparation/make_confounder.r
+++ 1-data_preparation/make_confounder.r
@@ -12,15 +12,21 @@ confs = read.table(paste(dataDir,'/Derived/exposure_confounder_variables_subset.
 
 # min mean max age
 min(confs$x21022_0_0, na.rm = TRUE)
+#38
 max(confs$x21022_0_0, na.rm = TRUE)
+#73
 mean(confs$x21022_0_0, na.rm = TRUE)
-
+#56.70664
+sd(confs$x21022_0_0, na.rm = TRUE)
+#8.057128
 
 # number of each sex
 unique(confs$x31_0_0)
+#0 1 (0=Female, 1=Male)
 length(which(confs$x31_0_0==0))
+#210,924
 length(which(confs$x31_0_0==1))
-
+#179503
 
 ## save confounder file - adjusting for age and sex
 

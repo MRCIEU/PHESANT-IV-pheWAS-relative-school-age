cb6e3ccdee689a8377bcd5016b980611806e5c33
MeldeLange <bo18534@bristol.ac.uk>
2022-04-04 14:38:09 +0100

    Updated to remove those withdrawn from UK Biobank

--- 1-data_preparation/restrict_sample.r
+++ 1-data_preparation/restrict_sample.r
@@ -1,12 +1,16 @@
-#Restricting sample to just those born in England
+#Restricting sample to who haven't withdrawn & just those born in England
 
 dataDir = Sys.getenv("PROJECT_DATA")
 
 variables = read.table(paste(dataDir,'/Derived/exposure_confounder_variables.csv', sep=""), sep=',', header=1)
 print ("dim of original data")
 print(dim(variables))
-# 502,448
+# 502,448  11
 
+withdrawn = read.table(paste(dataDir,'/Original/meta.withdrawn.20220222.csv', sep=""), sep=',', header=FALSE)
+print ("dim of withdrawn data")
+print(dim(withdrawn))
+#234 1
 
 #Restrict to participants in England
 
@@ -14,6 +18,17 @@ variables = subset(variables,(x1647_0_0 == 1))
 print ("dim of born in england subset")
 print(dim(variables))
 #390,454
+
+
+#Remove cases that have withdrawn from UK Biobank
+which(variables$eid %in% withdrawn$V1)
+#27
+variables <- variables[ ! variables$eid %in% withdrawn$V1, ]
+print ("dim of cases not withdrawn data")
+print(dim(variables))
+#390427 11
+#27 cases were removed
+
 
 
 #Check whether any participants missing age

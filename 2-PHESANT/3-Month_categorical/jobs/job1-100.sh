479b9e084c08a053ddf9f7953130285eb19bf865
MeldeLange <bo18534@bristol.ac.uk>
2022-04-05 10:01:37 +0100

    Updagted job time limit

--- 2-PHESANT/3-Month_categorical/jobs/job1-100.sh
+++ 2-PHESANT/3-Month_categorical/jobs/job1-100.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --time=01:00:00
+#SBATCH --time=03:00:00
 #SBATCH --mem=100G
 #SBATCH --array=1-100
 echo 'phesant_exposure2'

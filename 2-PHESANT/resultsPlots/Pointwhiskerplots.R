2831eb3a4cabaa28158c8ce39ae87a6fae8e25c1
MeldeLange <bo18534@bristol.ac.uk>
2022-06-13 13:01:49 +0100

    Updated to read.delim

--- 2-PHESANT/resultsPlots/Pointwhiskerplots.R
+++ 2-PHESANT/resultsPlots/Pointwhiskerplots.R
@@ -1,7 +1,7 @@
 resDir = Sys.getenv("RES_DIR")
 library("ggplot2")
 
-exp2_data <- read.table(resDir, '/phesant/exposure2/allres.csv', header = TRUE, sep= ",")
+exp2_data<-read.delim(paste(resDir,'/phesant/exposure2/allres.csv', sep=""), sep=',', header=1)
 head(exp2_data)
 
 
@@ -12,9 +12,9 @@ for (i in 1:numrows){
   varName <- exp2_data[i,"varName"]
 
 Month <- c("Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Oct", "Nov", "Dec")
-  Beta <- c(exp2_data[i,"beta_Jan"],exp2_data[i,"beta_Feb"], exp2_data[i,"beta_Mar"], exp2_data[i,"beta_Apr"], exp2_data[i,"beta_May"], exp2_data[i,"beta_Jun"], exp2_data[i,"beta_Jul"], exp2_data[i,"beta_Aug"], exp2_data[i,"beta_Oct"], exp2_data[i,"beta_Nov"], exp2_data[i,"beta_Dec"] )
-  lowci <-c(exp2_data[i,"ciL_Jan"],exp2_data[i,"ciL_Feb"], exp2_data[i,"ciL_Mar"], exp2_data[i,"ciL_Apr"], exp2_data[i,"ciL_May"], exp2_data[i,"ciL_Jun"], exp2_data[i,"ciL_Jul"], exp2_data[i,"ciL_Aug"], exp2_data[i,"ciL_Oct"], exp2_data[i,"ciL_Nov"], exp2_data[i,"ciL_Dec"])
-  upci <- c(exp2_data[i,"ciU_Jan"],exp2_data[i,"ciU_Feb"], exp2_data[i,"ciU_Mar"], exp2_data[i,"ciU_Apr"], exp2_data[i,"ciU_May"], exp2_data[i,"ciU_Jun"], exp2_data[i,"ciU_Jul"], exp2_data[i,"ciU_Aug"], exp2_data[i,"ciU_Oct"], exp2_data[i,"ciU_Nov"], exp2_data[i,"ciU_Dec"])
+Beta <- c(exp2_data[i,"beta_Jan"],exp2_data[i,"beta_Feb"], exp2_data[i,"beta_Mar"], exp2_data[i,"beta_Apr"], exp2_data[i,"beta_May"], exp2_data[i,"beta_Jun"], exp2_data[i,"beta_Jul"], exp2_data[i,"beta_Aug"], exp2_data[i,"beta_Oct"], exp2_data[i,"beta_Nov"], exp2_data[i,"beta_Dec"] )
+lowci <-c(exp2_data[i,"ciL_Jan"],exp2_data[i,"ciL_Feb"], exp2_data[i,"ciL_Mar"], exp2_data[i,"ciL_Apr"], exp2_data[i,"ciL_May"], exp2_data[i,"ciL_Jun"], exp2_data[i,"ciL_Jul"], exp2_data[i,"ciL_Aug"], exp2_data[i,"ciL_Oct"], exp2_data[i,"ciL_Nov"], exp2_data[i,"ciL_Dec"])
+upci <- c(exp2_data[i,"ciU_Jan"],exp2_data[i,"ciU_Feb"], exp2_data[i,"ciU_Mar"], exp2_data[i,"ciU_Apr"], exp2_data[i,"ciU_May"], exp2_data[i,"ciU_Jun"], exp2_data[i,"ciU_Jul"], exp2_data[i,"ciU_Aug"], exp2_data[i,"ciU_Oct"], exp2_data[i,"ciU_Nov"], exp2_data[i,"ciU_Dec"])
     
   df <-data.frame(Month, Beta, lowci, upci)
   print(df)

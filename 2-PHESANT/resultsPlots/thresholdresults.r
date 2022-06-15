7d73f903e730f1a01f2b8dfdce1910ee8ef1d2f5
MeldeLange <bo18534@bristol.ac.uk>
2022-06-15 13:40:35 +0100

    Added code to save file

--- 2-PHESANT/resultsPlots/thresholdresults.r
+++ 2-PHESANT/resultsPlots/thresholdresults.r
@@ -8,5 +8,8 @@ print(dim(comb_res))
 #Create subset with exp1 pvalue under 3.50E-06 or exp2 pvalue under 2.26E-06 or exp3 pvalue under 2.23E-06
 threshsub <-subset(comb_res, pvalue_exp1 < 3.50E-06 | pvalue_exp2 <2.26E-06 | pvalue_exp3 < 2.23E-06)
 print(dim(threshsub))
-head(thressub)
+#49, 59
+head(threshsub)
  
+#Save threshold results file
+write.table(threshsub, paste(resDir,'/phesant/thresholdsubset.csv',sep=""), sep=',', row.names=FALSE, quote = FALSE)

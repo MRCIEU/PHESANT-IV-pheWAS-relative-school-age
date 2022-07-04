36a0b56e9c2481261fe5d8f3a3668c4dc91de52b
MeldeLange <bo18534@bristol.ac.uk>
2022-07-04 14:20:31 +0100

    Script to check error in profile.polr

---
+++ 2-PHESANT/3-Month_categorical/jobs/Error_in_profile.polr.r
@@ -0,0 +1,40 @@
+#FOR X3829: Error "in optim"
+
+dataDir = Sys.getenv("PROJECT_DATA")
+
+#Read in exposure, confounders & outcome data
+exposure = read.table(paste(dataDir,'Derived/exposure2_month_indicator_variables.csv', sep=""), sep=',', header=1)
+head(exposure)
+confs = read.table(paste(dataDir,'Derived/confounders-sex_age.csv', sep=""), sep=',', header=1)
+head(confs)
+phenotype = read.table(paste(dataDir,'Original/phesant_saved-20211117/data-catord-9-200.txt', sep=""), sep=',', header=1)
+head(phenotype)
+
+#Just select the one outcome variable we want.
+phenotype = phenotype[ ,c("userID", "X1458")]
+
+**Change column name "userID" to "eid" in phenotype dataframe
+colnames(phenotype)
+names(phenotype)[names(phenotype) == "userID"] <- "eid"
+
+#Merge exposure & confounders into one dataframe
+print(dim(exposure))
+print(dim(confs))
+exp_confs <- merge(exposure,confs, by="eid", all.x = TRUE, all.y=TRUE)
+print(dim(exp_confs))
+head(exp_confs)
+
+#Merge phenotype with exposure/confounders
+print(dim(phenotype))
+data <- merge(phenotype,exp_confs, by="eid", all.x = TRUE, all.y=TRUE)
+print(dim(data))
+head(data)
+
+#Load MASS library to get polr function
+library (MASS)
+
+#Louise's regression code
+#fit <- polr(phenoFactor ~ ., data=confsPlusExp, Hess=TRUE)
+
+#fitB <- polr(phenoFactor ~ ., data=confs, Hess=TRUE)
+

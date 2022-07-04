b1c4876358b8437a611248b8775dc7a33a18c538
MeldeLange <bo18534@bristol.ac.uk>
2022-07-04 12:12:23 +0100

    Merged dataframes

--- 2-PHESANT/1-AUG_SEP/Error_in_if.r
+++ 2-PHESANT/1-AUG_SEP/Error_in_if.r
@@ -1,14 +1,45 @@
-#FOR X2375: Error "in if"
-
 dataDir = Sys.getenv("PROJECT_DATA")
 
+#Read in exposure, confounders & outcome data 
 exposure = read.table(paste(dataDir,'Derived/exposure1-Sep_Aug.csv', sep=""), sep=',', header=1)
 head(exposure)
 confs = read.table(paste(dataDir,'Derived/confounders-sex_age.csv', sep=""), sep=',', header=1)
 head(confs)
 phenotype = read.table(paste(dataDir,'Original/phesant_saved-20211117/data-catord-12-200.txt', sep=""), sep=',', header=1)
 head(phenotype)
-phenotype = phenotype[ ,c("userID", "2375")]
 
-fit <- polr(phenoFactor ~ ., data=confsPlusExp, Hess=TRUE)
-fitB <- polr(phenoFactor ~ ., data=confs, Hess=TRUE)
+#Just select the one outcome variable we want.
+phenotype = phenotype[ ,c("userID", "X2375")]
+
+**Change column name "userID" to "eid" in phenotype dataframe
+colnames(phenotype)
+names(phenotype)[names(phenotype) == "userID"] <- "eid"
+ 
+#Merge exposure & confounders into one dataframe
+print(dim(exposure))
+#64075     2
+print(dim(confs))
+#390427      3
+exp_confs <- merge(exposure,confs, by="eid", all.x = TRUE, all.y=TRUE)
+print(dim(exp_confs))
+#390427      4
+head(exp_confs)
+#eid Sep_Aug x31_0_0 x21022_0_0
+
+#Merge phenotype and  exposure/confounders into one dataframe
+print(dim(phenotype))
+#502448      2
+data <- merge(phenotype,exp_confs, by="eid", all.x = TRUE, all.y=TRUE)
+print(dim(data))
+#502448      5
+head(data)
+#eid X2375 Sep_Aug x31_0_0 x21022_0_0
+
+#Load MASS library to get polr function
+library (MASS)
+
+
+#Louise's regression code
+#fit <- polr(phenoFactor ~ ., data=confsPlusExp, Hess=TRUE)
+
+#fitB <- polr(phenoFactor ~ ., data=confs, Hess=TRUE)

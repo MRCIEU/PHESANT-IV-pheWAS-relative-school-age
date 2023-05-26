cd76b19a24b9abeb3d3587fc211c1a7377075a8c
louise <louise.millard@gmail.com>
2023-05-26 10:22:19 +0100

    add job files

---
+++ 3-follow_up/jthreshold.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=jthresholdresults
+#SBATCH --partition=veryshort
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=02:00:00
+#SBATCH --mem=100G
+#SBATCH --account=xxxxx
+
+
+#-----------------------------------------------------------------------------------------------------
+
+cd $SLURM_SUBMIT_DIR
+
+module load languages/r/4.1.0
+
+export RES_DIR="${HOME}/2022SchoolAge/results"
+
+
+date
+
+Rscript thresholdresults.r
+ 
+date
+

9f6e973afa47c31f96ff47a53479ff661324cd2d
louise <louise.millard@gmail.com>
2023-05-26 10:28:36 +0100

    bonf threshold update

--- 3-follow_up/thresholdresults.r
+++ 3-follow_up/thresholdresults.r
@@ -5,10 +5,10 @@ comb_res<-read.delim(paste(resDir,'/phesant/combinedresults.csv', sep=""), sep='
 head(comb_res)
 print(dim(comb_res))
 
-#Create subset with exp1 pvalue under 3.50E-06 or exp2 pvalue under 2.26E-06 or exp3 pvalue under 2.23E-06
-threshsub <-subset(comb_res, pvalue_exp1 < 3.50E-06 | pvalue_exp2 <2.26E-06 | pvalue_exp3 < 2.23E-06)
+#Create subset with exp1 pvalue under 3.33E-06 or exp3 pvalue under 2.16E-06
+threshsub <-subset(comb_res, pvalue_exp1 < 3.33E-06 | pvalue_exp3 < 2.16E-06)
 print(dim(threshsub))
-#49, 59
+
 head(threshsub)
  
 #Save threshold results file
